71268e80226bdb3aaaa09eb87e0da1f77543d6e5
Maxim Kazakov <kazakov.java@gmail.com>
2022-04-10 08:46:47 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.6.20-M1"
+}
+
+allprojects {
+    group = "org.algorithms"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+//        implementation(kotlin("stdlib"))
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "Algorithms_and_data_structures"
+
+include("1_Loops_and_recursion")
